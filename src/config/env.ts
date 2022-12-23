e53318c5a3dd053326772d057a7f2daabf54798d
Katlyn Lorimer <katlyn@katlyn.dev>
2022-12-22 22:27:26 -0700

    feat: automatically archive inactive QOTD threads

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,4 @@
-import { secret, strictVerify } from "env-verifier"
+import { secret, strictVerify, transform } from "env-verifier"
 
 const env = strictVerify({
   token: secret("DISCORD_TOKEN"),
@@ -7,6 +7,10 @@ const env = strictVerify({
   },
   roles: {
     member: "ROLE_MEMBER"
+  },
+  qotd: {
+    channels: transform("QOTD_CHANNELS",v => v.split(/(,\w*)/)),
+    maxAge: transform("QOTD_MAX_AGE", Number)
   }
 })
 
---
+++ src/events/qotdArchive.ts
@@ -0,0 +1,54 @@
+import { Base, Client } from "oceanic.js"
+
+import env from "../config/env"
+
+const ARCHIVE_INTERVAL = 60 * 1000
+
+const init = async (client: Client): Promise<void> => {
+  // Convert max age from minutes to ms
+  const maxInactiveTime = env.qotd.maxAge * 60 * 1000
+
+  const archiveTask = async () => {
+    // The initial date that all threads will be compared to
+    const now = new Date()
+
+    // Since threads aren't cached how we'd expect, fetch the active threads for each guild and iterate over the results
+    for (const [ _, guild ] of client.guilds) {
+
+      const activeThreads = await guild.getActiveThreads()
+
+      for (const thread of activeThreads.threads) {
+        // Check if the thread is in a QOTD channel
+        if (!env.qotd.channels.includes(thread.parentID)) {
+          continue
+        }
+
+        // If there's for some reason no messages in the thread, use its creation date as the timestamp for last
+        // activity
+        const lastId = thread.lastMessageID ?? thread.id
+        const lastActive = Base.getCreatedAt(lastId)
+
+        // Get the millis since the thread was last active
+        const timeSinceActive = now.getTime() - lastActive.getTime()
+
+        // If the thread has been inactive longer than the allowed time, archive it.
+        if (timeSinceActive > maxInactiveTime) {
+          console.log(`[QOTD Archive] Archived ${thread.name} (${thread.id})`)
+          await thread.edit({
+            archived: true
+          })
+        }
+      }
+    }
+  }
+
+  // Run the archive task on a set interval
+  setInterval(archiveTask, ARCHIVE_INTERVAL)
+
+  // Run the task once the bot is ready
+  client.once("ready", archiveTask)
+}
+
+export default {
+  init
+}
