1d6dc1763d9cb1f0f2605743b7e1721301331d2e
Katlyn Lorimer <katlyn@katlyn.dev>
2023-01-22 01:57:09 -0900

    feat: add event listener for interactions

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,12 +1,14 @@
 import { Client } from "oceanic.js"
 
 import guildCreate from "./guildCreate"
+import interactions from "./interactions"
 import memberVerified from "./memberVerified"
 import owoCount from "./owoCount"
 import qotdArchive from "./qotdArchive"
 
 const init = (client: Client): void => {
   guildCreate.init(client)
+  interactions.init(client)
   memberVerified.init(client)
   owoCount.init(client)
   qotdArchive.init(client)
--- src/events/interactions.ts
+++ src/events/interactions.ts
@@ -3,9 +3,10 @@ import { Client, InteractionTypes } from "oceanic.js"
 import InteractionHandler from "../util/interactionHandler"
 
 const init = (client: Client): void => {
+  client.once("ready", () => InteractionHandler.sync(client))
   client.on("interactionCreate", async interaction => {
     if (interaction.type === InteractionTypes.APPLICATION_COMMAND) {
-      console.log(`[Interactions / ${interaction.data.name}]: Issued by ${interaction.user.tag} (${interaction.user.id})`)
+      console.log(`[Interactions/${interaction.data.name}]: Issued by ${interaction.user.tag} (${interaction.user.id})`)
       await InteractionHandler.handle(client, interaction)
     }
 
