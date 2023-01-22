16965dc6e7de675dc49f8db417edf8f0bb25b03f
Katlyn Lorimer <katlyn@katlyn.dev>
2023-01-22 00:54:52 -0900

    feat: add listener for interactions

---
+++ src/events/interactions.ts
@@ -0,0 +1,18 @@
+import { Client, InteractionTypes } from "oceanic.js"
+
+import InteractionHandler from "../util/interactionHandler"
+
+const init = (client: Client): void => {
+  client.on("interactionCreate", async interaction => {
+    if (interaction.type === InteractionTypes.APPLICATION_COMMAND) {
+      console.log(`[Interactions / ${interaction.data.name}]: Issued by ${interaction.user.tag} (${interaction.user.id})`)
+      await InteractionHandler.handle(client, interaction)
+    }
+
+    // TODO: Handle autocomplete interactions
+  })
+}
+
+export default {
+  init
+}
