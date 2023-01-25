55c7bb7cb0617145708da60868c75ea4a55c2261
Katlyn Lorimer <katlyn@katlyn.dev>
2023-01-25 12:56:16 -0900

    feat: all slash command for owocount

--- src/commands/owoCount.ts
+++ src/commands/owoCount.ts
@@ -1,9 +1,44 @@
-import { ApplicationCommandTypes, CommandInteraction, MessageFlags } from "oceanic.js"
+import { ApplicationCommandBuilder } from "@oceanicjs/builders"
+import {
+  ApplicationCommandOptionTypes,
+  ApplicationCommandTypes,
+  CommandInteraction,
+  MessageFlags,
+  User
+} from "oceanic.js"
 
 import env from "../config/env"
 import prisma from "../config/prisma"
 import { isUser } from "../util/guards"
 import InteractionHandler, { InteractionCommand } from "../util/interactionHandler"
+
+async function sendResponse (interaction: CommandInteraction, user?: User) {
+  if (!isUser(user)) {
+    await interaction.createMessage({
+      content: "I couldn't find that user! This is probably a bug.",
+      flags: MessageFlags.EPHEMERAL
+    })
+    return
+  }
+  const userData = await prisma.user.findFirst({
+    where: {
+      id: BigInt(user.id)
+    }
+  })
+
+  let content = userData == null
+    ? `${user.mention} has never said owo or uwu! Are they really even a furry?`
+    : `${user.mention} has owo'd ${userData.owos} times and uwu'd ${userData.uwus} times.`
+
+  if (interaction.channelID !== env.channels.bot) {
+    content += `\nWant this message to be visible by everyone? Use the command in <#${env.channels.bot}>!`
+  }
+
+  await interaction.createMessage({
+    content,
+    flags: interaction.channelID !== env.channels.bot ? MessageFlags.EPHEMERAL : 0
+  })
+}
 
 class OwoCount extends InteractionCommand {
   description = ""
@@ -11,40 +46,33 @@ class OwoCount extends InteractionCommand {
   type = ApplicationCommandTypes.USER
 
   async run (interaction: CommandInteraction) {
-    if (!isUser(interaction.data.target)) {
-      await interaction.createMessage({
-        content: "I couldn't find that user! This is probably a bug.",
-        flags: MessageFlags.EPHEMERAL
-      })
-      return
-    }
-    const userData = await prisma.user.findFirst({
-      where: {
-        id: BigInt(interaction.data.target.id)
-      }
+    await sendResponse(interaction, interaction.data.target as User)
+  }
+}
+
+class ChatOwoCount extends InteractionCommand {
+  description = "See how many times a user has said owo or uwu"
+  name = "owocount"
+  type = ApplicationCommandTypes.CHAT_INPUT
+
+  setOptions (command: ApplicationCommandBuilder) {
+    command.addOption({
+      type: ApplicationCommandOptionTypes.USER,
+      name: "user",
+      description: "The user to look up",
+      required: false
     })
+  }
 
-    let channelMessage = ""
-    if (interaction.channelID !== env.channels.bot) {
-      channelMessage = `\nWant this message to be visible by everyone? Use the command in <#${env.channels.bot}>!`
-    }
-
-    if (userData == null) {
-      await interaction.createMessage({
-        content: `${interaction.data.target.mention} has never said owo or uwu! Are they really even a furry?${channelMessage}`,
-        flags: interaction.channelID !== env.channels.bot ? MessageFlags.EPHEMERAL : 0
-      })
-    } else {
-      await interaction.createMessage({
-        content: `${interaction.data.target.mention} has owo'd ${userData.owos} times and uwu'd ${userData.uwus} times.${channelMessage}`,
-        flags: interaction.channelID !== env.channels.bot ? MessageFlags.EPHEMERAL : 0
-      })
-    }
+  async run (interaction: CommandInteraction) {
+    const user = interaction.data.options.getUser("user") ?? interaction.user
+    await sendResponse(interaction, user)
   }
 }
 
 const init = () => {
   InteractionHandler.register(OwoCount)
+  InteractionHandler.register(ChatOwoCount)
 }
 
 export default {
