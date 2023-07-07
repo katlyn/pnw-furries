b60c3cdf15296e074b7443874686d165ef942f07
Katlyn <katlyn@katlyn.dev>
2023-07-07 15:10:05 -0600

    fix: don't crash if previous intro message deleted

--- src/events/introductionMessage.ts
+++ src/events/introductionMessage.ts
@@ -4,11 +4,11 @@ import env from "../config/env"
 import prisma from "../config/prisma"
 
 const introMessage: CreateMessageOptions = {
-  "content": "Welcome to Pacific Northwest Furries!",
-  "embeds": [
+  content: "Welcome to Pacific Northwest Furries!",
+  embeds: [
     {
-      "description": "Those waiting on verification, please double check the <#380463528483487744> to make sure you have everything, and don't forget to grab <#611658298420363274>!",
-      "color": 0x0c8b2e
+      description: "Those waiting on verification, please double check the <#380463528483487744> to make sure you have everything, and don't forget to grab <#611658298420363274>!",
+      color: 0x0c8b2e
     }
   ]
 }
@@ -17,7 +17,11 @@ const init = (client: Client): void => {
     if (message.channelID === env.channels.introduction && !message.author.bot) {
       const oldMessage = await prisma.introMessage.findFirst()
       if (oldMessage !== null) {
-        await client.rest.channels.deleteMessage(env.channels.introduction, oldMessage.id.toString())
+        try {
+          await client.rest.channels.deleteMessage(env.channels.introduction, oldMessage.id.toString())
+        } catch (e) {
+          console.error("Unable to remove previous intro message", e)
+        }
       }
       const newMessage = await client.rest.channels.createMessage(env.channels.introduction, introMessage)
       await prisma.introMessage.upsert({
