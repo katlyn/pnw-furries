f45c71cbe0b64a28d87920b449bf0cbb064fe936
Katlyn Lorimer <katlyn@katlyn.dev>
2023-09-17 15:40:56 -0800

    feat: add healthchecks and better error handling

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,35 @@
+import type { Server as HttpServer } from "node:http"
+import type { Http2SecureServer, Http2Server } from "node:http2"
+import type { Server as HttpsServer } from "node:https"
+
 import commands from "./commands"
 import client from "./config/client"
 import events from "./events/index"
 
+void (async () => {
+  process.on("unhandledRejection", (err, promise) => console.error("Unhandled Rejection:", err, promise))
+  process.on("uncaughtException", err => console.error("Uncaught Exception:", err))
 
-// Exit (somewhat) gracefully
-process.on("SIGTERM", () => {
-  client.disconnect(false)
-})
-client.on("error", () => {
-  client.disconnect(false)
-})
-// FIXME: Errors and unhandled rejections still need to be caught and handled gracefully. For now, we're just letting
-//  the bot crash.
+  // Exit (somewhat) gracefully
+  function gracefulExit (signal: string) {
+    client.disconnect(false)
+    statusServer?.close()
+    process.kill(process.pid, signal)
+  }
+  process.on("SIGINT", gracefulExit)
+  process.on("SIGTERM", gracefulExit)
 
-;(async () => {
+  // Register all event handlers
   commands.init()
   events.init(client)
 
-  await client.once("ready", () => {
-    console.log(`[INFO] Connected to Discord as ${client.user.tag ?? "unknown application"}. Using ${client.shards.size} shards.`)
-  })
-    .connect()
+  await client.connect()
+
+  type AnyServer = HttpServer | HttpsServer | Http2Server | Http2SecureServer
+  let statusServer: AnyServer|undefined
+  if (process.env.DOCKER === "TRUE") {
+    console.log("[INFO] Docker detected, starting status server")
+    const { default: StatusServer } = await import("@uwu-codes/status-server")
+    statusServer = StatusServer(() => client.ready)
+  }
 })()
-  .catch(console.error)
